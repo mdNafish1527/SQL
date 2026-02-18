d9d77d0c9829daa92ae0d5d2d282852b79c6eb94
mdNafish1527 <166948117+mdNafish1527@users.noreply.github.com>
2026-02-19 03:15:32 +0600

    Create Basics_Of_WHERE.sql

---
+++ Basics_Of_WHERE.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Office(
+  Name VARCHAR(10) NOT NULL DEFAULT 'Dhaka',
+  Number_of_Emp INT NOT NULL DEFAULT 0,
+  Unique_ID INT,
+  PRIMARY KEY(Unique_ID)
+);
+
+INSERT INTO Office (Name,Number_of_Emp,Unique_ID) VALUES 
+('Rajshahi',12,2010),
+('Dhaka',21,312);
+
+
+SELECT * FROM Office;
+
+CREATE TABLE Employee(
+  Name VARCHAR(11) NOT NULL,
+  Account_ID INT,
+  FOREIGN KEY (Account_ID) REFERENCES Office (Unique_ID)
+  );
+  
+INSERT INTO Employee (Name,Account_ID) VALUES
+('Nafish',312),
+('Nafish',312),
+('Nafish',312),
+('Montakim',312),
+('Arusha',312),
+('Montakim',312);
+
+SELECT * FROM Employee WHERE Name <> "Nafish";
