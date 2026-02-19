d5cd307748ad453c5b7facc4c9b99c972141ff22
mdNafish1527 <166948117+mdNafish1527@users.noreply.github.com>
2026-02-19 12:48:00 +0600

    Add SIR table with initial data and updates
    Create SIR table, insert records, and update names.

---
+++ Update.sql
@@ -0,0 +1,17 @@
+CREATE TABLE SIR(
+	Name VARCHAR(10) NOT NULL,
+    AGE INT DEFAULT 0,
+    Subject VARCHAR(10)
+);
+
+INSERT INTO SIR (Name,Age,Subject) VALUES
+('Rezvi',12,'Chemistry'),
+('Shoyaib',32,'Physics'),
+('Zarin',32,'Physics');
+
+SELECT * FROM SIR;
+UPDATE SIR SET Name = 'M B'
+WHERE Subject = 'Physics';
+
+SELECT * FROM SIR;
+        
